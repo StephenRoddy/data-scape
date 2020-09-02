6a45efdadc185708fc5386bfd89d8cc78f30f571
Stephen Roddy <roddyst@tcd.ie>
2020-09-02 17:27:30 +0100

    Updates

---
+++ precache-manifest.59576f618a30ee7800fb11c885c5abcc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c4128f65a8c85c8e724fffcad0e89f0",
+    "url": "/data-scape/index.html"
+  },
+  {
+    "revision": "6a7cd8056bdad39d33cd",
+    "url": "/data-scape/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "afeaf9b772f7cd2cee52",
+    "url": "/data-scape/static/css/main.7d6672d7.chunk.css"
+  },
+  {
+    "revision": "6a7cd8056bdad39d33cd",
+    "url": "/data-scape/static/js/2.dd6e04e7.chunk.js"
+  },
+  {
+    "revision": "4c861c5bcb6761152cc7d8fc1865096b",
+    "url": "/data-scape/static/js/2.dd6e04e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afeaf9b772f7cd2cee52",
+    "url": "/data-scape/static/js/main.037e792c.chunk.js"
+  },
+  {
+    "revision": "c7cc69f7c0d63ddfb9f5",
+    "url": "/data-scape/static/js/runtime-main.72f7ab0d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/data-scape/static/media/logo.5d5d9eef.svg"
+  }
+]);
