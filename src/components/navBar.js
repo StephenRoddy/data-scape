e7480b7518a7af8344425c5acf838a1471e1fcb4
Stephen Roddy <roddyst@tcd.ie>
2020-09-02 17:44:12 +0100

    small changes

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Navbar = ( props ) => {
 return (
-  <nav class="navbar mb-4 navbar-light">
+  <nav class="navbar mb-4 navbar-light shadow-sm ml-2 mr-2 mt-2">
+
     <a class="navbar-brand" href="#"><h3>Multimodal Data-analytics</h3><h5 class ="text-wrap">API data from COVID-19</h5></a>
   </nav>
   )
