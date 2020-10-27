4a6ea2ec288ab814e17f014dfe76a13abd8a5270
Stephen Roddy <roddyst@tcd.ie>
2020-10-27 16:50:06 +0000

    Update navBar.js

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -4,7 +4,7 @@ const Navbar = ( props ) => {
 return (
   <nav class="navbar mb-4 navbar-light shadow-sm ml-2 mr-2 mt-2">
 
-    <a class="navbar-brand" href="#"><h3>COVID-19 Sonification Framework</h3><h5 class ="text-wrap">API data from COVID-19</h5></a>
+  <a class="navbar-brand" href="#"><h3>Sonification Framework for Ireland's Covid-19 Outbreak</h3> <a href ="https://covid19ireland-geohive.hub.arcgis.com/" target="_blank"> <h5 class ="text-wrap">Data from Ireland's COVID-19 Data Hub</h5></a></a>
   </nav>
   )
 };
