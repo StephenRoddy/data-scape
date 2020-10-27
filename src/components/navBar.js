e54ca64345eabeb3706bc07b7f1c84b7694060ca
Stephen Roddy <roddyst@tcd.ie>
2020-10-27 16:47:45 +0000

    Update navBar.js

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -4,7 +4,7 @@ const Navbar = ( props ) => {
 return (
   <nav class="navbar mb-4 navbar-light shadow-sm ml-2 mr-2 mt-2">
 
-    <a class="navbar-brand" href="#"><h3>Multimodal Data-analytics</h3><h5 class ="text-wrap">API data from COVID-19</h5></a>
+    <a class="navbar-brand" href="#"><h3>COVID-19 Sonification Framework</h3><h5 class ="text-wrap">API data from COVID-19</h5></a>
   </nav>
   )
 };
